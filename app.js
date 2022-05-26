fdcee53ce2998302531670a51f9217098bbcffc0
smhoke <80280099+smhoke@users.noreply.github.com>
2022-05-25 21:28:56 -0400

    local date and time

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 let apiKey = "c0d34ff35f99187b8089dbc8ea55c072";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Troy,US&appid=${apiKey}&units=imperial`;
 
+let now = new Date();
+
+//current date
+//day
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+//month
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+//date
+let date = now.getDate();
+
+let currentDate = document.querySelector("#currentDate");
+currentDate.innerHTML = `${day}, ${month} ${date}`;
+
+//current time
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let currentTime = document.querySelector("#currentTime");
+currentTime.innerHTML = `${hours}:${minutes}`;
+
 function displayDefault(response) {
   console.log(response.data);
   let currentCityElement = document.querySelector("#currentCity");
